2d3f35606f57a88efe59aaecddd0d76fbe3743f1
judychui <judychui331@gmail.com>
2018-06-05 12:01:25 +0200

    Add files via upload

---
+++ pythi.py
@@ -0,0 +1,22 @@
+#myString = "Alpha Beta Gamma"
+#myString.split()
+#['Alpha', 'Beta', 'Gamma']
+#myString = myString.split(" ")
+#    myString[2]
+#    myString.index("Alpha")
+#    type(myString)
+#   <class 'list'>
+#
+myString = "hello lol laugh sunny".split(" ")
+if "hello" in myString:
+    print("bye")
+    print(myString[1])
+else:
+    print("heyyy")
+
+#def f(x,y):
+    if x in y:
+        print("Yea it is!")
+    else:
+        print("sorry no!")
+#f("bye","hello world")
